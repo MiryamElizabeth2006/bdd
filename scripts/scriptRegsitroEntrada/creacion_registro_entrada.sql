cd0fd67d699e4f1193b2555d286799becc136d74
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-03-07 15:10:14 -0500

    my sql tablas realizada reto 1 modulo 2

---
+++ scripts/scriptRegsitroEntrada/creacion_registro_entrada.sql
@@ -0,0 +1,9 @@
+ -- creación tabla 4
+ 
+ create table registros_entrada(
+ 	codigo_registro int not null,
+	 cedula_empleado char (10) not null,
+	 fecha date not null,
+	 hora time not null,
+	 constraint registro_entrada_pk primary key(codigo_registro)
+ )
