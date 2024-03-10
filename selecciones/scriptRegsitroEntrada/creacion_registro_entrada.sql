c80c9942ea01338897311dcfeab24ccdec213fe8
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-03-10 16:48:57 -0500

    Los scrpits correctos puestos en la carpeta Selecciones

--- selecciones/scriptRegsitroEntrada/creacion_registro_entrada.sql
+++ selecciones/scriptRegsitroEntrada/creacion_registro_entrada.sql
@@ -1,4 +1,4 @@
- -- creación tabla 4
+  -- creación tabla 4
  
  create table registros_entrada(
  	codigo_registro int not null,
@@ -7,3 +7,40 @@
 	 hora time not null,
 	 constraint registro_entrada_pk primary key(codigo_registro)
  )
+ insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+ values (00010,'1748756912','02/02/2024','12:12') 
+ 
+ insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+ values (00011,'0603215478','7/03/2024','14:20')
+ 
+ insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+ values (00012,'1478945212','06/02/2024','06:12')
+ 
+ insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+ values (00013,'1478945212','12/02/2024','8:20') 
+ 
+ insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+ values (00014,'1524789632','2/03/2024','9:30') 
+ 
+ insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+ values (00015,'0745841278','18/02/2024','11:15')  
+ 
+ insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+ values (00016,'1714527836','02/03/2024','10:22') 
+ 
+ insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+ values (00017,'0623547822','08/11/2023','11:12') 
+ 
+ insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+ values (00018,'4578961234','25/12/2023','16:30') 
+ 
+ insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+ values (00019,'1748756924','07/11/2023','13:00') 
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+ values (00020,'1848756988','09/05/2023','12:40') 
+ 
+ select * from registros_entrada
+ 
+ 
+ 
--- selecciones/scrpitProductos/creacion_productos.sql
+++ selecciones/scrpitProductos/creacion_productos.sql
@@ -8,3 +8,30 @@ create table productos(
 	stock int not null,
 	constraint productos_pk primary key (codigo)
 )
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (0001,'pan','producto de panaderia',0.25,50)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (0002,'yogurt','producto de lacteos',0.10,100)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (0003,'Café','Negro Buendía',1.50,300)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (0004,'Helados','pingüino grande pastel',3.50,150)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (0005,'Limonada','Refrescante 2Lt',1.00,200)
+
+insert into productos (codigo,nombre,precio,stock)
+values (0006,'Jamón',0.90,150)
+
+insert into productos (codigo,nombre,precio,stock)
+values (0007,'Queso',2.00,120)
+
+insert into productos (codigo,nombre,precio,stock)
+values (0008,'Milanesa',3.50,170)
+
+select * from productos
+
+
